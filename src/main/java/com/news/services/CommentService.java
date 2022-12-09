0e62e7904daf161213ad04a76ea61052367d19b5
Andrey Novikov <andresderes2002@gmail.com>
2022-12-10 00:11:20 +0300

    fix query

--- src/main/java/com/news/services/CommentService.java
+++ src/main/java/com/news/services/CommentService.java
@@ -7,6 +7,10 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -112,9 +116,47 @@ public class CommentService {
     }
 
     public boolean deleteAll(Long idPost) {
-        return commentDAO.deleteAllByIdPost(idPost);
+        try (Connection connection = get();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("DELETE FROM comment WHERE id_post = " + idPost)){
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private Connection get() throws SQLException {
+        String address, login, password;
+        address = login = password = null;
+
+        try(BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("./src/main/resources/application.properties")
+        )) {
+            int index = 0;
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                index++;
+
+                if (index == 3) {
+                    address = line.split("=")[1];
+                }
+                if (index == 4) {
+                    login = line.split("=")[1];
+                }
+                if (index == 5) {
+                    password = line.split("=")[1];
+                }
+                if (index == 6) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert address != null;
+        return DriverManager.getConnection(address, login, password);
+    }
     private @NotNull Comment nullComment() {
         Comment comment = new Comment();
 
